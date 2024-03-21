0d0966377ccfc1b34d81d51a6c59a4f7e51e3c7e
Bezawait <bezawitedilu@gmail.com>
2024-03-21 12:54:08 +0300

    Update User model and composer dependencies to use Laravel Breeze
    - Move User model to App\Models\Models namespace
    - Remove unnecessary use statements
    - Update composer.json to require laravel/breeze
    - Remove duplicate laravel/breeze requirement from composer.lock
    
    Note: The User model was previously modified, but the changes were reverted in this diff.

--- app/Models/EventRegistration.php
+++ app/Models/EventRegistration.php
@@ -5,7 +5,7 @@ namespace App\Models\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class EventRegistration extends Model
 {
     use HasFactory;
 }
